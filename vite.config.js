90b8e86c60471255eb4d14ebe49a8ef1291d33f4
isabelioli <oliveiraisabeli888@gmail.com>
2025-11-13 15:20:54 -0300

    Add setup instructions and definitions to vite.config.js
    Updated vite.config.js with setup instructions and definitions.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,61 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+English Activity - Folder Setup on Git/Terminal
+Activity 1:
+.First, make sure the tools are installed:
+-Node.js (including npm)
+-Code editor (e.g., VSCode)
+-Terminal (Can be VS terminal, etc.)
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-})
+.Check if npm and Node.js are installed:
+-Step 1:
+node -v
+npm -v
+-Step 2:
+ex: npm create ingles@latest my-activity
+-Step 3:
+(access the project folder)
+cd my-activity
+-Step 4:
+(install dependencies)
+npm install
+-Step 5:
+(start the project)
+npm run dev
+(This command will start the project server)
+
+.Verification:
+(You need to access the application in the browser)
+http://localhost:5173
+
+Activity 2:
+.C:\Users\1066218\Pictures\Screenshots\Captura de Tela (1).png
+(Web application running successfully in the browser)
+
+.C:\Users\1066218\Pictures\Screenshots\Captura de Tela (2).png
+(To create the app)
+
+.C:\Users\1066218\Pictures\Screenshots\Captura de Tela (3).png
+(Terminal showing the installation of project dependencies using npm)
+
+.C:\Users\1066218\Pictures\Screenshots\Captura de Tela (4).png
+(Create the project)
+
+.C:\Users\1066218\Pictures\Screenshots\Captura de Tela (5).png
+(Development server successfully started and running on localhost)
+
+.C:\Users\1066218\Pictures\Screenshots\Captura de Tela (6).png
+(Application result)
+
+Activity 3:
+Term (English) | Tradução (Português)| Definition (English)
+  .Node.js | Node.js  |A JavaScript runtime built on Chrome's V8 engine, used to run JavaScript on the server.
+  . npm | npm (Node Package Manager)| A package manager for Node.js that helps install and manage project dependencies.
+  .Dependency | Dependência | A library or module that a project needs to function properly.
+  .Terminal | Terminal | A command-line interface used to interact with the operating system and run commands.
+  .Development |Server Servidor de Desenvolvimento | A local server used to preview and test the application during development.
+  .localhost |localhost |A hostname that refers to the local computer,typically used for testing web apps.
+  .Framework | Framework | A set of tools and libraries that provide structure for building applications.
+  .React  | React  |A JavaScript library for building user interfaces,especially single-page applications.
+  .Vite | Vite | A build tool that provides fast development and bundling for modern web projects.
+  .JavaScript | JavaScript | A programming language commonly used to create interactive effects in web browsers
+     
+      
